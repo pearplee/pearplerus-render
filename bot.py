b7807d5ed979746938d2b41bddddaecba60bc03f
Denis <yaroslavnesterovb@gmail.com>
2025-03-11 19:42:10 +0700

    Обновил WEBHOOK_URL

--- bot.py
+++ bot.py
@@ -4,6 +4,8 @@ from telebot import types
 import os
 
 
+API_TOKEN = '7637282666:AAH6fwKJxMKIs6FPixJs49AzkqGNKYMzrlM'
+WEBHOOK_URL = 'https://pearpleeng-render.onrender.com'
 bot = telebot.TeleBot(os.environ.get('7637282666:AAH6fwKJxMKIs6FPixJs49AzkqGNKYMzrlM'))
 app = Flask(__name__)
 
